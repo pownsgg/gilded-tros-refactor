279e8eb7acfcab70b5686946df64cc57bfc024c2
pownsgg <broitspowns@gmail.com>
2025-03-21 16:40:46 +0100

    Added lombok to project + fixed field access of classes

--- src/main/java/com/gildedtros/GildedTros.java
+++ src/main/java/com/gildedtros/GildedTros.java
@@ -1,64 +1,70 @@
 package com.gildedtros;
 
-class GildedTros {
-    Item[] items;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-    public GildedTros(Item[] items) {
-        this.items = items;
-    }
+@AllArgsConstructor
+@Data
+class GildedTros {
+    private final Item[] items;
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Good Wine")
-                    && !items[i].name.equals("Backstage passes for Re:Factor")
-                    && !items[i].name.equals("Backstage passes for HAXX"))
-            {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("B-DAWG Keychain")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+	    for (Item item : items) {
+            String itemName = item.getName();
+            int itemQuality = item.getQuality();
+            int itemSellIn = item.getSellIn();
 
-                    if (items[i].name.equals("Backstage passes for Re:Factor") || items[i].name.equals("Backstage passes for HAXX") ) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+		    if (!itemName.equals("Good Wine")
+			    && !itemName.equals("Backstage passes for Re:Factor")
+			    && !itemName.equals("Backstage passes for HAXX")) {
+			    if (itemQuality > 0) {
+				    if (!itemName.equals("B-DAWG Keychain")) {
+					    itemQuality = itemQuality - 1;
+				    }
+			    }
+		    } else {
+			    if (itemQuality < 50) {
+				    itemQuality = itemQuality + 1;
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+				    if (itemName.equals("Backstage passes for Re:Factor") || itemName.equals("Backstage passes for HAXX")) {
+					    if (itemSellIn < 11) {
+						    if (itemQuality < 50) {
+							    itemQuality = itemQuality + 1;
+						    }
+					    }
 
-            if (!items[i].name.equals("B-DAWG Keychain")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+					    if (itemSellIn < 6) {
+						    if (itemQuality < 50) {
+							    itemQuality = itemQuality + 1;
+						    }
+					    }
+				    }
+			    }
+		    }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Good Wine")) {
-                    if (!items[i].name.equals("Backstage passes for Re:Factor") && !items[i].name.equals("Backstage passes for HAXX")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("B-DAWG Keychain")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
+		    if (!itemName.equals("B-DAWG Keychain")) {
+			    itemSellIn = itemSellIn - 1;
+		    }
+
+		    if (itemSellIn < 0) {
+			    if (!itemName.equals("Good Wine")) {
+				    if (!itemName.equals("Backstage passes for Re:Factor") && !itemName.equals("Backstage passes for HAXX")) {
+					    if (itemQuality > 0) {
+						    if (!itemName.equals("B-DAWG Keychain")) {
+							    itemQuality = itemQuality - 1;
+						    }
+					    }
+				    } else {
+					    itemQuality = itemQuality - itemQuality;
+				    }
+			    } else {
+				    if (itemQuality < 50) {
+					    itemQuality = itemQuality + 1;
+				    }
+			    }
+		    }
+
+            // TODO: Update item objects
+	    }
     }
 }
