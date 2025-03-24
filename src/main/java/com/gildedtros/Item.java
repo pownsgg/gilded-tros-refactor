e14121a634b1a29677e9731d382e5c663cbfdad6
pownsgg <broitspowns@gmail.com>
2025-03-24 12:06:13 +0100

    Added ItemType enum and field to Item class

--- src/main/java/com/gildedtros/Item.java
+++ src/main/java/com/gildedtros/Item.java
@@ -4,9 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class Item {
     private String name;
+    private ItemType itemType;
     private int sellIn;
     private int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        this(name, ItemType.REGULAR, sellIn, quality);
+    }
+
+    public Item(String name, ItemType itemType, int sellIn, int quality) {
+        this.name = name;
+        this.itemType = itemType;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
 }
---
+++ src/main/java/com/gildedtros/ItemType.java
@@ -0,0 +1,8 @@
+package com.gildedtros;
+
+public enum ItemType {
+	AGED_QUALITY_INCREASE,
+	BACKSTAGE_PASS,
+	LEGENDARY,
+	REGULAR
+}
